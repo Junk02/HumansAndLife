845d50140a2cbbf80fff1893311f05fc69129a21
Junk02 <kleshchukevichart@gmail.com>
2023-06-23 12:48:37 +0300

    Создал класс Map

---
+++ HumansAndLife_project/HumansAndLife_project/Map.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumansAndLife_project
+{
+    internal class Map
+    {
+        public int x_size, y_size;
+
+        public Map(int x, int y)
+        {
+            x_size = x;
+            y_size = y;
+        }
+    }
+}
